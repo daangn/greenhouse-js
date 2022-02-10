915ce79c710967c675375ac974e6b3fa3eeb47fb
Hyeseong Kim <tim@daangn.com>
2022-02-11 02:33:44 +0900

    gatsby-source-greenhouse-jobboard: fix types (#18)

---
+++ .changeset/flat-eagles-burn.md
@@ -0,0 +1,5 @@
+---
+"@karrotmarket/gatsby-source-greenhouse-jobboard": minor
+---
+
+Fix type definition for Gatsby nodes
--- packages/@karrotmarket/gatsby-source-greenhouse-jobboard/src/types.ts
+++ packages/@karrotmarket/gatsby-source-greenhouse-jobboard/src/types.ts
@@ -1,4 +1,4 @@
-import type { Node, NodeInput } from 'gatsby';
+import type { Node } from 'gatsby';
 import type {
   Job,
   JobContentFields,
@@ -14,22 +14,22 @@ export type PluginOptions = {
 
 export type GreenhouseJobBoardJobNodeSource = (
   & Omit<Job & JobContentFields & JobQuestionFields, 'id'>
-  & { ghId: number; boardToken: string }
+  & { ghId: number, boardToken: string }
 );
 
 export type GreenhouseJobBoardJobNode = (
   & Node
   & Omit<Job & JobContentFields & JobQuestionFields, 'id'>
-  & { ghId: number; boardToken: string }
+  & { ghId: string, boardToken: string }
 );
 
 export type GreenhouseJobBoardDepartmentNodeSource = (
   & Omit<Department & DepartmentListItem, 'id'>
-  & { ghId: number; boardToken: string }
+  & { ghId: number, boardToken: string }
 );
 
 export type GreenhouseJobBoardDepartmentNode = (
   & Node
   & Omit<Department & DepartmentListItem, 'id'>
-  & { ghId: number; boardToken: string }
+  & { ghId: string, boardToken: string }
 );
